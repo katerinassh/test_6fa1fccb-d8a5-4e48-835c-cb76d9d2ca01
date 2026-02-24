ab8c6dd2a268b3f1bba2fbba9966a625d46679ff
Kateryna <k.shakirianova@viseven.com>
2026-02-25 00:49:31 +0200

    add logger

--- src/probation/infrastructure/http/probation-api.client.ts
+++ src/probation/infrastructure/http/probation-api.client.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 import { ProbationClient } from '../../interfaces/probation-client.interface';
 import { FetchCampaignReports } from '../../../common/interfaces/fetch-campaign-reports.interface';
 
 @Injectable()
 export class ProbationApiClient implements ProbationClient {
+  private readonly logger = new Logger(ProbationApiClient.name);
+
   constructor(private readonly http: HttpService) {}
 
   fetchCampaignReports(params: FetchCampaignReports) {
-    return this.http
-      .get('/tasks/campaign/reports', { params })
-      .pipe(map((res) => res.data.data.csv));
+    this.logger.debug(`Requesting Probation API with params: ${JSON.stringify(params)}`);
+  
+    return this.http.get('/tasks/campaign/reports', { params }).pipe(
+      map((res) => res.data.data.csv),
+      catchError((error) => {
+        this.logger.error(`Failed to fetch reports: ${error.message}`);
+        
+        return throwError(() => error);
+      })
+    );
   }
 }
